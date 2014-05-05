46904ad5e685b1524006e1cbe3e528955928479f
Fenn <pokefenn@googlemail.com>
2014-05-05 20:12:34 +0100

    There, gradle things~~~

--- src/main/java/info/jbcs/minecraft/chisel/Chisel.java
+++ src/main/java/info/jbcs/minecraft/chisel/Chisel.java
@@ -37,7 +37,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.GameRegistry.Type;
 
 
-@Mod(modid = "chisel", name = "Chisel", version = "1.5.4a")
+@Mod(modid = "chisel", name = "Chisel", version = "1.5.4b")
 public class Chisel
 {
     public static ItemChisel chisel;
