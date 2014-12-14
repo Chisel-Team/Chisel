e6bb0e6e875bc3e64e2fcb16c551772eda66a716
TheCricket <bcord@me.com>
2014-12-14 14:22:17 -0500

    Added Grimstone and Fantasy Block 2

---
+++ src/main/java/info/jbcs/minecraft/chisel/block/BlockGrimstone.java
@@ -0,0 +1,35 @@
+package info.jbcs.minecraft.chisel.block;
+
+import info.jbcs.minecraft.chisel.Chisel;
+import info.jbcs.minecraft.chisel.client.GeneralChiselClient;
+import info.jbcs.minecraft.chisel.init.ModTabs;
+import info.jbcs.minecraft.chisel.utils.General;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+public class BlockGrimstone extends BlockCarvable {
+    public IIcon iconStar;
+
+    public BlockGrimstone(Material m) {
+        super(m);
+        this.setCreativeTab(ModTabs.tabChiselBlocks);
+    }
+
+    @Override
+    public void randomDisplayTick(World world, int x, int y, int z, Random random) {
+        if (General.rand.nextInt(4) == 0 && world.isRemote)
+            GeneralChiselClient.spawnGrimstoneFX(world, this, x, y, z);
+    }
+
+    @Override
+    public void registerBlockIcons(IIconRegister register) {
+        super.registerBlockIcons(register);
+
+        iconStar = register.registerIcon(Chisel.MOD_ID + ":grimstone/particles/star");
+    }
+
+}
