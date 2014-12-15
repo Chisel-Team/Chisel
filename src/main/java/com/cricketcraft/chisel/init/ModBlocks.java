a7cb158e1a6a489c62054064b6b38cf2bdc77c1e
Arona Jones <warlordjones@gmail.com>
2014-12-15 17:05:41 +0000

    More mechanism/technical blocks. Pending split into two blocks

--- src/main/java/com/cricketcraft/chisel/init/ModBlocks.java
+++ src/main/java/com/cricketcraft/chisel/init/ModBlocks.java
@@ -1450,6 +1450,12 @@ public class ModBlocks {
             mechanical.carverHelper.addVariation(StatCollector.translateToLocal("tile.mechanical.1.desc"), 1, "mechanical/cautiontape");
             mechanical.carverHelper.addVariation(StatCollector.translateToLocal("tile.mechanical.2.desc"), 2, "mechanical/relic");
             mechanical.carverHelper.addVariation(StatCollector.translateToLocal("tile.mechanical.3.desc"), 3, "mechanical/wires");
+            mechanical.carverHelper.addVariation(StatCollector.translateToLocal("tile.mechanical.4.desc"), 4, "mechanical/fan");
+            mechanical.carverHelper.addVariation(StatCollector.translateToLocal("tile.mechanical.5.desc"), 5, "mechanical/vent");
+            mechanical.carverHelper.addVariation(StatCollector.translateToLocal("tile.mechanical.6.desc"), 6, "mechanical/vent-glow");
+            mechanical.carverHelper.addVariation(StatCollector.translateToLocal("tile.mechanical.7.desc"), 7, "mechanical/hex");
+            mechanical.carverHelper.addVariation(StatCollector.translateToLocal("tile.mechanical.8.desc"), 8, "mechanical/gear");
+            mechanical.carverHelper.addVariation(StatCollector.translateToLocal("tile.mechanical.9.desc"), 9, "mechanical/vent2");
             mechanical.carverHelper.register(mechanical, "mechanical");
             Carving.chisel.registerOre("mechanical", "mechanical");
         }
