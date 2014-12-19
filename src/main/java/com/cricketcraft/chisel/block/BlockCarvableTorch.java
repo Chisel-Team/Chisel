33be12cdedaef8204f8fa88354b110c963e2c6f1
Arona Jones <warlordjones@gmail.com>
2014-12-19 10:21:52 +0000

    Chiselable Torches :D

--- src/main/java/com/cricketcraft/chisel/block/BlockCarvableTorch.java
+++ src/main/java/com/cricketcraft/chisel/block/BlockCarvableTorch.java
@@ -1,11 +1,18 @@
 package com.cricketcraft.chisel.block;
 
+import net.minecraft.block.BlockTorch;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
+import com.cricketcraft.chisel.Chisel;
+import com.cricketcraft.chisel.api.ICarvable;
 import com.cricketcraft.chisel.carving.CarvableHelper;
+import com.cricketcraft.chisel.carving.CarvableVariation;
 import com.cricketcraft.chisel.init.ModTabs;
 
-public class BlockCarvableTorch extends BlockCarvable
+public class BlockCarvableTorch extends BlockTorch implements ICarvable
 {
     public CarvableHelper carverHelper;
+    private String texLocation;
 
     public BlockCarvableTorch()
     {
@@ -15,9 +22,15 @@ public class BlockCarvableTorch extends BlockCarvable
 
         setCreativeTab(ModTabs.tabChiselBlocks);
     }
-    @Override
-    public int getRenderType()
-    {
-        return 2;
+	@Override
+	public CarvableVariation getVariation(int metadata) {
+		return carverHelper.getVariation(metadata);
+	}
+	@Override
+    public void registerBlockIcons(IIconRegister icon){
+        this.blockIcon = icon.registerIcon(Chisel.MOD_ID + ":torch/"+ texLocation);
+    }
+	public void setInformation(String textureLocation){
+        this.texLocation = textureLocation;
     }
 }
