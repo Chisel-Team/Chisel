8620d975ee16f657b79260b94f28a2088b2316d3
Cricket <bcord@me.com>
2014-12-23 08:12:47 -0800

    API Note addded

--- src/main/java/com/cricketcraft/chisel/api/ICarvable.java
+++ src/main/java/com/cricketcraft/chisel/api/ICarvable.java
@@ -4,5 +4,11 @@ import com.cricketcraft.chisel.carving.CarvableVariation;
 
 public interface ICarvable
 {
+    /**
+     * This grabs the variations possible
+     * @param metadata This is the metadata that a block will use and the basis of chisel
+     * Traditionally you make an instance of CarvableHelper ex. CarvableHelper carverHelper = new CarvableHelper();
+     * In this method you return carverHelper.getVariation(metadata) and that should be all you need if you register it correctly.
+     */
     public CarvableVariation getVariation(int metadata);
 }
