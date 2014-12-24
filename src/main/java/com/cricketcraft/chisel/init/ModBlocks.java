291043567b8e42431feddd2d8732b26ad64db012
Drullkus <ericsplanet@mac.com>
2014-12-24 09:03:14 -0800

    Add other Arcane Stone block

--- src/main/java/com/cricketcraft/chisel/init/ModBlocks.java
+++ src/main/java/com/cricketcraft/chisel/init/ModBlocks.java
@@ -1564,9 +1564,10 @@ public class ModBlocks {
          {
              arcane = (BlockCarvable) new BlockCarvable(Material.rock).setStepSound(Block.soundTypeStone);
              Carving.chisel.addVariation("arcane", GameRegistry.findBlock("Thaumcraft", "blockCosmeticSolid"), 6, 0);
-             arcane.carverHelper.addVariation(StatCollector.translateToLocal("tile.arcane.1.desc"), 1, "arcane/largetile");
-             arcane.carverHelper.addVariation(StatCollector.translateToLocal("tile.arcane.2.desc"), 2, "arcane/moon");
-             arcane.carverHelper.addVariation(StatCollector.translateToLocal("tile.arcane.3.desc"), 3, "arcane/moon-thaumium");
+             Carving.chisel.addVariation("arcane", GameRegistry.findBlock("Thaumcraft", "blockCosmeticSolid"), 7, 1);
+             arcane.carverHelper.addVariation(StatCollector.translateToLocal("tile.arcane.1.desc"), 0, "arcane/largetile");
+             arcane.carverHelper.addVariation(StatCollector.translateToLocal("tile.arcane.2.desc"), 1, "arcane/moon");
+             arcane.carverHelper.addVariation(StatCollector.translateToLocal("tile.arcane.3.desc"), 2, "arcane/moon-thaumium");
              arcane.carverHelper.register(arcane, "arcane");
              Carving.chisel.registerOre("arcane", "arcane");
          }
