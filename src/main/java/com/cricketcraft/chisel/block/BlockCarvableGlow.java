d394a9a6e7c29ff50ee226ca57e766542f68f193
tterrag1098 <tterrag1098@gmail.com>
2015-01-08 18:36:23 -0500

    Add missing files

---
+++ src/main/java/com/cricketcraft/chisel/block/BlockCarvableGlow.java
@@ -0,0 +1,41 @@
+package com.cricketcraft.chisel.block;
+
+import com.cricketcraft.chisel.Chisel;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.util.IIcon;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class BlockCarvableGlow extends BlockCarvable {
+
+	@SideOnly(Side.CLIENT)
+	private IIcon glowTexture;
+
+	private String glowTexturePath;
+
+	public BlockCarvableGlow(String glowTexture) {
+		super();
+		this.glowTexturePath = glowTexture;
+	}
+
+	@Override
+	public void registerBlockIcons(IIconRegister register) {
+		super.registerBlockIcons(register);
+		glowTexture = register.registerIcon(Chisel.MOD_ID + ":" + glowTexturePath);
+	}
+
+	public IIcon getGlowTexture() {
+		return glowTexture;
+	}
+
+	@Override
+	public int getRenderType() {
+		return Chisel.renderGlowId;
+	}
+
+	@Override
+	public boolean renderAsNormalBlock() {
+		return false;
+	}
+}
