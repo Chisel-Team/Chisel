0ce5e507dc7d8cd78bf510a069f2214ed1b56238
Mitchellbrine <awsomeman500@gmail.com>
2015-01-11 11:28:24 -0800

    Added the tool required to break road lines
    Set it as a wood pickaxe. Still breaks instantly, but it cannot be broken in Adventure Mode. Can be configured to any tool you desire (except shears, in which case, just make it implement IShearable).

--- src/main/java/com/cricketcraft/chisel/block/BlockRoadLine.java
+++ src/main/java/com/cricketcraft/chisel/block/BlockRoadLine.java
@@ -19,6 +19,7 @@ public class BlockRoadLine extends Block {
 	public BlockRoadLine() {
 		super(Material.circuits);
 
+		this.setHarvestLevel("pickaxe",0);
 		this.setBlockBounds(0.0f, 0.0f, 0.0f, 1.0f, 0.00390625f, 1.0f);
 		// this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.0625F, 1.0F);
 	}
