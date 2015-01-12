2020424e135e94d4b5aa44f08c2ef370260a38a1
tterrag1098 <tterrag1098@gmail.com>
2015-01-11 23:30:10 -0500

    Fix buildscript for dev builds and unify version variable

--- src/main/java/com/cricketcraft/chisel/Chisel.java
+++ src/main/java/com/cricketcraft/chisel/Chisel.java
@@ -66,7 +66,7 @@ public class Chisel {
 	public static final String MOD_ID = "chisel";
 	public static final BlockCarvable.SoundType soundTempleFootstep = new BlockCarvable.SoundType("dig.stone", MOD_ID + ":step.templeblock", 1.0f, 1.0f);
 	public static final String MOD_NAME = "Chisel 2";
-	public static final String VERSION = "2.2.2";
+	public static final String VERSION = "@VERSION@";
 	public static final BlockCarvable.SoundType soundHolystoneFootstep = new BlockCarvable.SoundType("holystone", 1.0f, 1.0f);
 	public static final BlockCarvable.SoundType soundMetalFootstep = new BlockCarvable.SoundType("metal", 1.0f, 1.0f);
 	public static boolean multipartLoaded = false;
