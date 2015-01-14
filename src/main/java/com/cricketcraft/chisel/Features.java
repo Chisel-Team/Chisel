83c3d1c85ca162f4f9fd84dc92972e73bb32b4c9
tterrag1098 <tterrag1098@gmail.com>
2015-01-14 06:23:52 -0500

    Fix some corner cases in slab placement

--- src/main/java/com/cricketcraft/chisel/Features.java
+++ src/main/java/com/cricketcraft/chisel/Features.java
@@ -2572,7 +2572,7 @@ public enum Features {
 	
 	private static void registerSlabTop(Block bottom, Block top) {
 		String name = Block.blockRegistry.getNameForObject(bottom);
-		name = name.substring(name.indexOf(':')) + "_top";
+		name = name.substring(name.indexOf(':') + 1) + "_top";
 		GameRegistry.registerBlock(top, ItemCarvableSlab.class, name);
 	}
 }
