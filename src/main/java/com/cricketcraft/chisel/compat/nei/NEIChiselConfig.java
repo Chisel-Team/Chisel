bdfc938e52110a71d0ae0d547ac17628c7e811dd
tterrag1098 <tterrag1098@gmail.com>
2015-01-14 05:45:12 -0500

    Merge the two IConfigureNEI classes into one

--- src/main/java/com/cricketcraft/chisel/compat/nei/NEIChiselConfig.java
+++ src/main/java/com/cricketcraft/chisel/compat/nei/NEIChiselConfig.java
@@ -1,9 +1,12 @@
 package com.cricketcraft.chisel.compat.nei;
 
+import net.minecraft.item.ItemStack;
+
 import com.cricketcraft.chisel.Chisel;
 
 import codechicken.nei.api.API;
 import codechicken.nei.api.IConfigureNEI;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class NEIChiselConfig implements IConfigureNEI {
 
@@ -19,6 +22,10 @@ public class NEIChiselConfig implements IConfigureNEI {
 
 	@Override
 	public void loadConfig() {
+		API.hideItem(new ItemStack(GameRegistry.findBlock("chisel", "limestone_slab_top")));
+		API.hideItem(new ItemStack(GameRegistry.findBlock("chisel", "marble_slab_top")));
+		API.hideItem(new ItemStack(GameRegistry.findBlock("chisel", "marble_pillar_slab_top")));
+		
 		RecipeHandlerChisel handler = new RecipeHandlerChisel();
 		API.registerRecipeHandler(handler);
 		API.registerUsageHandler(handler);
