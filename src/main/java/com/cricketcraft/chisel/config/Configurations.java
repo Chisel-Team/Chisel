d4bd181ef1e998f3230489a9372d8723ff69282d
tterrag1098 <tterrag1098@gmail.com>
2015-01-19 18:59:25 -0500

    Add some lifecycle logging to Features

--- src/main/java/com/cricketcraft/chisel/config/Configurations.java
+++ src/main/java/com/cricketcraft/chisel/config/Configurations.java
@@ -104,8 +104,8 @@ public class Configurations {
 		return config.get("features", featureName(feature), true).getBoolean(true) && refreshConfig();
 	}
 
-	// this makes the old camelCase names from the new CONSTANT_CASE names
-	private static String featureName(Features feature) {
+	/** Makes the old camelCase names from the new CONSTANT_CASE names */
+	public static String featureName(Features feature) {
 		String[] words = feature.name().toLowerCase(Locale.ENGLISH).split("_");
 		if (words.length == 1) {
 			return words[0];
