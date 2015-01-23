e446d54e7aaa2b4fa58bb78e2d9d98298ed540a5
tterrag1098 <tterrag1098@gmail.com>
2015-01-22 21:47:30 -0500

    Add ICarvingRegistry, still need to document properly

---
+++ src/main/java/com/cricketcraft/chisel/api/carving/ICarvingRegistry.java
@@ -0,0 +1,47 @@
+package com.cricketcraft.chisel.api.carving;
+
+import java.util.List;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+/**
+ * Represents a registry of {@link ICarvingGroup}s
+ * <p>
+ * To obtain chisel's instance of this class, use {@link CarvingUtils#getChiselRegistry()}
+ */
+public interface ICarvingRegistry {
+
+	/* Getters */
+
+	ICarvingGroup getGroup(Block block, int meta);
+
+	ICarvingGroup getGroup(String name);
+
+	ICarvingVariation getVariation(Block block, int meta);
+
+	List<ICarvingVariation> getGroupVariations(Block block, int meta);
+
+	String getOreName(Block block, int meta);
+
+	List<ItemStack> getItemsForChiseling(ItemStack chiseled);
+
+	public String getVariationSound(Block block, int metadata);
+
+	public String getVariationSound(Item item, int metadata);
+
+	List<String> getSortedGroupNames();
+
+	/* Setters */
+
+	void addVariation(String groupName, Block block, int metadata, int order);
+
+	void addVariation(String groupName, ICarvingVariation variation);
+
+	void addGroup(ICarvingGroup group);
+
+	void registerOre(String groupName, String oreName);
+
+	void setVariationSound(String name, String sound);
+}
