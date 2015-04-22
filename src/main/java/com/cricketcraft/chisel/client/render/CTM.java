85711e377eccd6699c9ebbf8f6f2e6d23cdb1416
tterrag1098 <tterrag1098@gmail.com>
2015-04-22 17:52:03 -0400

    Massively clean up CTM code, much more readable now

--- src/main/java/com/cricketcraft/chisel/client/render/CTM.java
+++ src/main/java/com/cricketcraft/chisel/client/render/CTM.java
@@ -1,13 +1,23 @@
 package com.cricketcraft.chisel.client.render;
 
-import com.cricketcraft.chisel.config.Configurations;
+import gnu.trove.map.TIntObjectMap;
+import gnu.trove.map.hash.TIntObjectHashMap;
+
+import java.util.EnumMap;
+
 import net.minecraft.block.Block;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.util.ForgeDirection;
 
 import com.cricketcraft.chisel.api.IFacade;
+import com.cricketcraft.chisel.config.Configurations;
+import com.google.common.collect.Maps;
+
+import static com.cricketcraft.chisel.client.render.CTM.Dir.*;
+import static net.minecraftforge.common.util.ForgeDirection.*;
 
 /**
+ * @formatter:off
  * The CTM renderer will draw the block's FACE using by assembling 4 quadrants from the 5 available block
  * textures.  The normal Texture.png is the blocks "unconnected" texture, and is used when CTM is disabled or the block
  * has nothing to connect to.  This texture has all of the outside corner quadrants  The texture-ctm.png contains the
@@ -56,129 +66,167 @@ import com.cricketcraft.chisel.api.IFacade;
  * HAVE FUN!
  * -CptRageToaster-
  */
-
 public class CTM {
-	public static int[] getSubmapIndices(IBlockAccess world, int x, int y, int z, int side) {
-		if (world == null)
-			return new int[] { 18, 19, 17, 16 };
 
-		Block block = world.getBlock(x, y, z);
-		int blockMetadata = world.getBlockMetadata(x, y, z);
+	enum Dir {
+		
+		TOP(UP), 
+		TOP_RIGHT(UP, EAST), 
+		RIGHT(EAST), 
+		BOTTOM_RIGHT(DOWN, EAST), 
+		BOTTOM(DOWN), 
+		BOTTOM_LEFT(DOWN, WEST), 
+		LEFT(WEST), 
+		TOP_LEFT(UP, WEST);
+		// @formatter:on
 
-		boolean b[] = new boolean[8];
-		/**
-		 * b[0]    b[1]    b[2]
-		 *
-		 *
-		 *
-		 * b[3]    FACE    b[4]
-		 *
-		 *
-		 *
-		 * b[5]    b[6]    b[7]
-		 */
-		if (side == 0) {
-			b[0] = isConnected(world, x - 1, y, z + 1, side, block, blockMetadata);
-			b[1] = isConnected(world, x, y, z + 1, side, block, blockMetadata);
-			b[2] = isConnected(world, x + 1, y, z + 1, side, block, blockMetadata);
-			b[3] = isConnected(world, x - 1, y, z, side, block, blockMetadata);
-			b[4] = isConnected(world, x + 1, y, z, side, block, blockMetadata);
-			b[5] = isConnected(world, x - 1, y, z - 1, side, block, blockMetadata);
-			b[6] = isConnected(world, x, y, z - 1, side, block, blockMetadata);
-			b[7] = isConnected(world, x + 1, y, z - 1, side, block, blockMetadata);
-		} else if (side == 1) {
-			b[0] = isConnected(world, x - 1, y, z - 1, side, block, blockMetadata);
-			b[1] = isConnected(world, x, y, z - 1, side, block, blockMetadata);
-			b[2] = isConnected(world, x + 1, y, z - 1, side, block, blockMetadata);
-			b[3] = isConnected(world, x - 1, y, z, side, block, blockMetadata);
-			b[4] = isConnected(world, x + 1, y, z, side, block, blockMetadata);
-			b[5] = isConnected(world, x - 1, y, z + 1, side, block, blockMetadata);
-			b[6] = isConnected(world, x, y, z + 1, side, block, blockMetadata);
-			b[7] = isConnected(world, x + 1, y, z + 1, side, block, blockMetadata);
-		} else if (side == 2) {
-			b[0] = isConnected(world, x + 1, y + 1, z, side, block, blockMetadata);
-			b[1] = isConnected(world, x, y + 1, z, side, block, blockMetadata);
-			b[2] = isConnected(world, x - 1, y + 1, z, side, block, blockMetadata);
-			b[3] = isConnected(world, x + 1, y, z, side, block, blockMetadata);
-			b[4] = isConnected(world, x - 1, y, z, side, block, blockMetadata);
-			b[5] = isConnected(world, x + 1, y - 1, z, side, block, blockMetadata);
-			b[6] = isConnected(world, x, y - 1, z, side, block, blockMetadata);
-			b[7] = isConnected(world, x - 1, y - 1, z, side, block, blockMetadata);
-		} else if (side == 3) {
-			b[0] = isConnected(world, x - 1, y + 1, z, side, block, blockMetadata);
-			b[1] = isConnected(world, x, y + 1, z, side, block, blockMetadata);
-			b[2] = isConnected(world, x + 1, y + 1, z, side, block, blockMetadata);
-			b[3] = isConnected(world, x - 1, y, z, side, block, blockMetadata);
-			b[4] = isConnected(world, x + 1, y, z, side, block, blockMetadata);
-			b[5] = isConnected(world, x - 1, y - 1, z, side, block, blockMetadata);
-			b[6] = isConnected(world, x, y - 1, z, side, block, blockMetadata);
-			b[7] = isConnected(world, x + 1, y - 1, z, side, block, blockMetadata);
-		} else if (side == 4) {
-			b[0] = isConnected(world, x, y + 1, z - 1, side, block, blockMetadata);
-			b[1] = isConnected(world, x, y + 1, z, side, block, blockMetadata);
-			b[2] = isConnected(world, x, y + 1, z + 1, side, block, blockMetadata);
-			b[3] = isConnected(world, x, y, z - 1, side, block, blockMetadata);
-			b[4] = isConnected(world, x, y, z + 1, side, block, blockMetadata);
-			b[5] = isConnected(world, x, y - 1, z - 1, side, block, blockMetadata);
-			b[6] = isConnected(world, x, y - 1, z, side, block, blockMetadata);
-			b[7] = isConnected(world, x, y - 1, z + 1, side, block, blockMetadata);
-		} else if (side == 5) {
-			b[0] = isConnected(world, x, y + 1, z + 1, side, block, blockMetadata);
-			b[1] = isConnected(world, x, y + 1, z, side, block, blockMetadata);
-			b[2] = isConnected(world, x, y + 1, z - 1, side, block, blockMetadata);
-			b[3] = isConnected(world, x, y, z + 1, side, block, blockMetadata);
-			b[4] = isConnected(world, x, y, z - 1, side, block, blockMetadata);
-			b[5] = isConnected(world, x, y - 1, z + 1, side, block, blockMetadata);
-			b[6] = isConnected(world, x, y - 1, z, side, block, blockMetadata);
-			b[7] = isConnected(world, x, y - 1, z - 1, side, block, blockMetadata);
+		private static final ForgeDirection NORMAL = SOUTH;
+		private static final Dir[] VALUES = values();
+
+		private ForgeDirection[] dirs;
+
+		private Dir(ForgeDirection... dirs) {
+			this.dirs = dirs;
 		}
 
+		private boolean isConnected(IBlockAccess world, int x, int y, int z, int sideIdx, Block block, int meta) {
+			ForgeDirection side = getOrientation(sideIdx);
+			ForgeDirection[] dirs = getNormalizedDirs(side);
+			for (ForgeDirection dir : dirs) {
+				x += dir.offsetX;
+				y += dir.offsetY;
+				z += dir.offsetZ;
+			}
+			return CTM.isConnected(world, x, y, z, side, block, meta);
+		}
+
+		private ForgeDirection[] getNormalizedDirs(ForgeDirection normal) {
+			if (normal == NORMAL) {
+				return dirs;
+			} else if (normal == NORMAL.getOpposite()) {
+				// If this is the opposite direction of the default normal, we need to mirror the dirs
+				// A mirror version does not affect y+ and y- so we ignore those
+				ForgeDirection[] ret = new ForgeDirection[dirs.length];
+				for (int i = 0; i < ret.length; i++) {
+					ret[i] = dirs[i].offsetY != 0 ? dirs[i] : dirs[i].getOpposite();
+				}
+				return ret;
+			} else {
+				ForgeDirection axis = null;
+				// Next, we need different a different rotation axis depending 
+				// on if this is up/down or not
+				if (normal.offsetY == 0) {
+					// If it is not up/down, pick either the left or right-hand rotation
+					axis = normal == NORMAL.getRotation(UP) ? UP : DOWN; 
+				} else {
+					// If it is up/down, pick either the up or down rotation.
+					axis = normal == UP ? NORMAL.getRotation(DOWN) : NORMAL.getRotation(UP);
+				}
+				ForgeDirection[] ret = new ForgeDirection[dirs.length];
+				// Finally apply all the rotations
+				for (int i = 0; i < ret.length; i++) {
+					ret[i] = dirs[i].getRotation(axis);
+				}
+				return ret;
+			}
+		}
+	}
+
+	/** Some hardcoded offset values for the different corner indeces */
+	private static int[] submapOffsets = { 4, 5, 1, 0 };
+	private static TIntObjectMap<Dir[]> submapMap = new TIntObjectHashMap<Dir[]>();
+	private static EnumMap<Dir, Boolean> connectionMap = Maps.newEnumMap(Dir.class);
+	static {
+		for (Dir dir : Dir.VALUES) {
+			connectionMap.put(dir, false);
+		}
+		// Mapping the different corner indeces to their respective dirs
+		submapMap.put(0, new Dir[] { BOTTOM, LEFT, BOTTOM_LEFT });
+		submapMap.put(1, new Dir[] { BOTTOM, RIGHT, BOTTOM_RIGHT });
+		submapMap.put(2, new Dir[] { TOP, RIGHT, TOP_RIGHT });
+		submapMap.put(3, new Dir[] { TOP, LEFT, TOP_LEFT });
+	}
+
+	public static int[] getSubmapIndices(IBlockAccess world, int x, int y, int z, int side) {
 		int[] ret = new int[] { 18, 19, 17, 16 };
 
-		// Bottom Left
-		if (b[3] || b[6]) {
-			ret[0] = 4 + (b[6] ? 2 : 0) + (b[3] ? 8 : 0);
-			if (b[3] && b[5] && b[6]) ret[0] = 4;
+		if (world == null) {
+			return ret;
 		}
+		
+		Block block = world.getBlock(x, y, z);
+		int meta = world.getBlockMetadata(x, y, z);
 
-		// Bottom Right
-		if (b[6] || b[4]) {
-			ret[1] = 5 + (b[6] ? 2 : 0) + (b[4] ? 8 : 0);
-			if (b[4] && b[6] && b[7]) ret[1] = 5;
+		// Build connections
+		for (Dir dir : Dir.VALUES) {
+			connectionMap.put(dir, dir.isConnected(world, x, y, z, side, block, meta));
 		}
-
-		// Top Right
-		if (b[4] || b[1]) {
-			ret[2] = 1 + (b[1] ? 2 : 0) + (b[4] ? 8 : 0);
-			if (b[1] && b[2] && b[4]) ret[2] = 1;
+		
+		// Map connections to submap indeces
+		for (int i = 0; i < 4; i++) {
+			fillSubmaps(ret, i);
 		}
-
-		// Top Left
-		if (b[1] || b[3]) {
-			ret[3] = (b[1] ? 2 : 0) + (b[3] ? 8 : 0);
-			if (b[0] && b[1] && b[3]) ret[3] = 0;
-		}
-
+		
 		return ret;
 	}
 
+	private static void fillSubmaps(int[] ret, int idx) {
+		Dir[] dirs = submapMap.get(idx);
+		if (connectedOr(dirs[0], dirs[1])) {
+			if (connectedAnd(dirs)) {
+				// If all dirs are connected, we use the fully connected face,
+				// the base offset value.
+				ret[idx] = submapOffsets[idx];
+			} else {
+				// This is a bit magic-y, but basically the array is ordered so
+				// the first dir requires an offset of 2, and the second dir
+				// requires an offset of 8, plus the initial offset for the
+				// corner.
+				ret[idx] = submapOffsets[idx] + (connected(dirs[0]) ? 2 : 0) + (connected(dirs[1]) ? 8 : 0);
+			}
+		}
+	}
+
+	private static boolean connected(Dir dir) {
+		return connectionMap.get(dir);
+	}
+	private static boolean connectedAnd(Dir... dirs) {
+		for (Dir dir : dirs) {
+			if (!connected(dir)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private static boolean connectedOr(Dir... dirs) {
+		for (Dir dir : dirs) {
+			if (connected(dir)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	public static boolean isConnected(IBlockAccess world, int x, int y, int z, int side, Block block, int meta) {
-
 		ForgeDirection dir = ForgeDirection.VALID_DIRECTIONS[side];
+		return isConnected(world, x, y, z, dir, block, meta);
+	}
 
+	public static boolean isConnected(IBlockAccess world, int x, int y, int z, ForgeDirection dir, Block block, int meta) {
 		int x2 = x + dir.offsetX;
 		int y2 = y + dir.offsetY;
 		int z2 = z + dir.offsetZ;
 
-		Block con = getBlockOrFacade(world, x, y, z, side);
-		Block obscuring = getBlockOrFacade(world, x2, y2, z2, side);
+		Block con = getBlockOrFacade(world, x, y, z, dir.ordinal());
+		Block obscuring = getBlockOrFacade(world, x2, y2, z2, dir.ordinal());
 
 		// no block or a bad API user
 		if (con == null) {
 			return false;
 		}
 
-		boolean ret = con.equals(block) && getBlockOrFacadeMetadata(world, x, y, z, side) == meta;
+		boolean ret = con.equals(block) && getBlockOrFacadeMetadata(world, x, y, z, dir.ordinal()) == meta;
 
 		// no block obscuring this face
 		if (obscuring == null) {
@@ -186,7 +234,7 @@ public class CTM {
 		}
 
 		// check that we aren't already connected outwards from this side
-		ret &= !(obscuring.equals(block) && getBlockOrFacadeMetadata(world, x2, y2, z2, side) == meta && Configurations.connectInsideCTM);
+		ret &= !(obscuring.equals(block) && getBlockOrFacadeMetadata(world, x2, y2, z2, dir.ordinal()) == meta && Configurations.connectInsideCTM);
 
 		return ret;
 	}
