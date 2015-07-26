31aeb6d85cabebc5a49e91c6fd0a6457dd69178d
tterrag1098 <tterrag1098@gmail.com>
2015-07-25 22:31:55 -0400

    Fix botania compat #379

--- src/main/java/com/cricketcraft/chisel/api/IConnectable.java
+++ src/main/java/com/cricketcraft/chisel/api/IConnectable.java
@@ -1,24 +1,26 @@
 package com.cricketcraft.chisel.api;
 
 import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.common.util.ForgeDirection;
 
 /**
  * This extension of {@link IFacade} allows the block to say whether or not OTHER CTM blocks can connect to IT.
  */
-public interface IConnectable extends IFacade {
+public interface IConnectable {
 
 	/**
 	 * Determines whether other CTM blocks can connect to this one.
 	 * 
 	 * @param world
 	 * @param x
+	 *            The X position of the block that is connecting to this one. NOT the position of your block.
 	 * @param y
+	 *            The Y position of the block that is connecting to this one. NOT the position of your block.
 	 * @param z
-	 * @param from
-	 *            The direction that the block is connecting from. This differes from the {@link IFacade} methods in that it is NOT the side which is being rendered.
+	 *            The Z position of the block that is connecting to this one. NOT the position of your block.
+	 * @param side
+	 *            The side being drawn.
 	 * @return True if a block can connect to this one from the given direction. False otherwise.
 	 */
-	boolean canConnectCTM(IBlockAccess world, int x, int y, int z, ForgeDirection from);
+	boolean canConnectCTM(IBlockAccess world, int x, int y, int z, int side);
 
 }
