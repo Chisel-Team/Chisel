d0cbf5ecac6169e423a67c9a506295a23e9b3833
tterrag1098 <tterrag1098@gmail.com>
2015-10-13 20:14:29 -0400

    Fix some crashes from disabling certain features

--- src/main/java/team/chisel/proxy/ClientProxy.java
+++ src/main/java/team/chisel/proxy/ClientProxy.java
@@ -8,6 +8,7 @@ import net.minecraft.item.Item;
 import net.minecraft.world.World;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.MinecraftForge;
+import team.chisel.Features;
 import team.chisel.block.BlockCarvableBeacon;
 import team.chisel.block.tileentity.TileEntityAutoChisel;
 import team.chisel.block.tileentity.TileEntityCarvableBeacon;
@@ -47,45 +48,57 @@ public class ClientProxy extends CommonProxy {
 		RenderingRegistry.registerBlockHandler(new RendererLayeredGlow());
 		RenderingRegistry.registerBlockHandler(new RendererSimpleLayered());
 		RenderingRegistry.registerBlockHandler(new RenderRoofing());
-
 		RenderingRegistry.registerBlockHandler(new RendererMultiLayer());
 
-		RenderAutoChisel rac = new RenderAutoChisel();
-		RenderingRegistry.registerBlockHandler(rac);
-		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ChiselBlocks.autoChisel), rac);
-		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityAutoChisel.class, rac);
+		if (Features.AUTO_CHISEL.enabled()) {
+			RenderAutoChisel rac = new RenderAutoChisel();
+			RenderingRegistry.registerBlockHandler(rac);
+			MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ChiselBlocks.autoChisel), rac);
+			ClientRegistry.bindTileEntitySpecialRenderer(TileEntityAutoChisel.class, rac);
+		}
 
-		RenderCarvableBeacon rcb = new RenderCarvableBeacon();
-		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityCarvableBeacon.class, rcb);
-		RenderingRegistry.registerBlockHandler(rcb);
-		BlockCarvableBeacon.renderId = rcb.getRenderId();
+		if (Features.BEACON.enabled()) {
+			RenderCarvableBeacon rcb = new RenderCarvableBeacon();
+			ClientRegistry.bindTileEntitySpecialRenderer(TileEntityCarvableBeacon.class, rcb);
+			RenderingRegistry.registerBlockHandler(rcb);
+			BlockCarvableBeacon.renderId = rcb.getRenderId();
+		}
 
-		RenderPresent rp = new RenderPresent();
-		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ChiselBlocks.present), rp);
-		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityPresent.class, rp);
+		if (Features.PRESENT.enabled()) {
+			RenderPresent rp = new RenderPresent();
+			MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ChiselBlocks.present), rp);
+			ClientRegistry.bindTileEntitySpecialRenderer(TileEntityPresent.class, rp);
+		}
 
-		RenderingRegistry.registerEntityRenderingHandler(EntityCloudInABottle.class, new RenderSnowball(ChiselItems.cloudinabottle));
-		RenderingRegistry.registerEntityRenderingHandler(EntityBallOMoss.class, new RenderSnowball(ChiselItems.ballomoss));
-		RenderingRegistry.registerEntityRenderingHandler(EntitySmashingRock.class, new RenderSnowball(ChiselItems.smashingrock));
-		RenderingRegistry.registerEntityRenderingHandler(EntityChiselSnowman.class, new RenderChiselSnowman());
+		if (Features.CLOUD.enabled()) {
+			RenderingRegistry.registerEntityRenderingHandler(EntityCloudInABottle.class, new RenderSnowball(ChiselItems.cloudinabottle));
+		}
+		if (Features.BALL_OF_MOSS.enabled()) {
+			RenderingRegistry.registerEntityRenderingHandler(EntityBallOMoss.class, new RenderSnowball(ChiselItems.ballomoss));
+		}
+		if (Features.SMASHING_ROCK.enabled()) {
+			RenderingRegistry.registerEntityRenderingHandler(EntitySmashingRock.class, new RenderSnowball(ChiselItems.smashingrock));
+		}
+		if (Features.PUMPKIN.enabled()) {
+			RenderingRegistry.registerEntityRenderingHandler(EntityChiselSnowman.class, new RenderChiselSnowman());
+		}
 
-		MinecraftForgeClient.registerItemRenderer(ChiselItems.chisel, renderer);
-		MinecraftForgeClient.registerItemRenderer(ChiselItems.diamondChisel, renderer);
+		if (Features.CHISEL.enabled()) {
+			MinecraftForgeClient.registerItemRenderer(ChiselItems.chisel, renderer);
+			MinecraftForgeClient.registerItemRenderer(ChiselItems.diamondChisel, renderer);
+		}
+
+		ItemStarFieldRenderer enderStarField = new ItemStarFieldRenderer();
+		if (Features.OFFSET_TOOL.enabled()) {
+			MinecraftForge.EVENT_BUS.register(ChiselItems.offsettool);
+			MinecraftForgeClient.registerItemRenderer(ChiselItems.offsettool, enderStarField);
+		}
+
 //		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ChiselBlocks.antiBlock), new SubmapManagerAntiblock.AntiblockItemRenderer());
-
-		MinecraftForge.EVENT_BUS.register(ChiselItems.offsettool);
 
         MinecraftForge.EVENT_BUS.register(new InterpolatedIcon.RegistrationHandler());
 
-        // Had 75% no idea what I was doing. Shaders, woo!
-
-        // TODO: Resource location "textures/entity/end_portal.png" Find out where the fuck to put this
-
         FMLCommonHandler.instance().bus().register(new ClientTickHandler());
-
-		ItemStarFieldRenderer enderStarField = new ItemStarFieldRenderer();
-
-		MinecraftForgeClient.registerItemRenderer(ChiselItems.offsettool, enderStarField);
 
 		ShaderHelper.initShaders();
 	}
