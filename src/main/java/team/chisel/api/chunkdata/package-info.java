b552e5e9c239b3a650599000e7b63bf408ffc4ec
tterrag1098 <tterrag1098@gmail.com>
2015-10-17 14:01:16 -0400

    Add package-info to chunkdata api package

---
+++ src/main/java/team/chisel/api/chunkdata/package-info.java
@@ -0,0 +1,6 @@
+@API(apiVersion = ChiselAPIProps.VERSION, owner = "chisel", provides = "ChiselAPI|ChunkData")
+package team.chisel.api.chunkdata;
+
+import team.chisel.api.ChiselAPIProps;
+import cpw.mods.fml.common.API;
+
