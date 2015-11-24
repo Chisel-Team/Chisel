75d7e05dc31cf6106670a28d638b230dbc0e8fcf
Drullkus <ericsplanet@mac.com>
2015-11-24 02:19:07 -0800

    Tweak the rainbow and chisel renderers on dev players

--- src/main/java/team/chisel/client/render/DevPlayer.java
+++ src/main/java/team/chisel/client/render/DevPlayer.java
@@ -31,8 +31,8 @@ import team.chisel.init.ChiselItems;
 public class DevPlayer {
 
     private boolean resetRender;
-    private float renderTick;
 
+    private float renderTick;
 
     private interface IDevRenderer {
         void renderExtras(EntityPlayer player, boolean post, RenderPlayerEvent.Specials event);
@@ -57,8 +57,8 @@ public class DevPlayer {
                 boolean armor = player.getCurrentArmor(1) != null;
                 GL11.glRotatef(180.0F, 1.0F, 0.0F, 0.0F);
                 GL11.glRotatef(90.0F, 0.0F, 1.0F, 0.0F);
-                GL11.glTranslatef(-0.25F, -0.85F, armor ? 0.2F : 0.28F);
-                GL11.glScalef(0.5F, 0.5F, 0.5F);
+                GL11.glTranslatef(-0.35F, -0.85F, armor ? 0.2F : 0.28F);
+                GL11.glScalef(0.6F, 0.6F, 0.6F);
                 GL11.glColor3f(1.0F, 1.0F, 1.0F);
                 int light = 0xF000F0;
                 int lightmapX = light % 0x10000;
@@ -73,28 +73,17 @@ public class DevPlayer {
         }
     }
 
-    private class RenderRainbow implements IDevRenderer {
-
-        @Override
-        public void renderExtras(EntityPlayer player, boolean post, RenderPlayerEvent.Specials event) {
-        }
-    }
-
     public static final DevPlayer INSTANCE = new DevPlayer();
 
     private Multimap<UUID, IDevRenderer> renderMap = ArrayListMultimap.create();
 
     private DevPlayer() {
-        RenderHolstered backChisel = new RenderHolstered(new ItemStack(ChiselItems.obsidianChisel));
-        RenderRainbow translucent = new RenderRainbow();
-
-
-        renderMap.putAll(UUID.fromString("a7529984-8cb2-4fb9-b799-97980f770101"), Lists.newArrayList(backChisel, translucent)); // Cricket
-        renderMap.putAll(UUID.fromString("a1d2532b-ee11-4ca3-b4c5-76e168d4c98e"), Lists.newArrayList(backChisel, translucent)); // TheCricket26
-        renderMap.putAll(UUID.fromString("5399b615-3440-4c66-939d-ab1375952ac3"), Lists.newArrayList(new RenderHolstered(new ItemStack(ChiselItems.diamondChisel)))); // Drullkus
+        renderMap.put(UUID.fromString("a7529984-8cb2-4fb9-b799-97980f770101"), new RenderHolstered(new ItemStack(ChiselItems.obsidianChisel))); // Cricket
+        renderMap.put(UUID.fromString("a1d2532b-ee11-4ca3-b4c5-76e168d4c98e"), new RenderHolstered(new ItemStack(ChiselItems.obsidianChisel))); // TheCricket26
+        renderMap.put(UUID.fromString("5399b615-3440-4c66-939d-ab1375952ac3"), new RenderHolstered(new ItemStack(ChiselItems.diamondChisel))); // Drullkus
 
         renderMap.put(UUID.fromString("671516b1-4fb3-4c03-aa7c-9c88cfab3ae8"), new RenderHolstered(new ItemStack(ChiselItems.diamondChisel))); // tterrag
-        renderMap.put(UUID.fromString("ad18f501-08fa-4e7e-b324-86750009106e"), new RenderHolstered(new ItemStack(ChiselItems.chisel)));//minecreatr
+        renderMap.put(UUID.fromString("ad18f501-08fa-4e7e-b324-86750009106e"), new RenderHolstered(new ItemStack(ChiselItems.chisel))); // minecreatr
 
         MinecraftForge.EVENT_BUS.register(this);
     }
@@ -105,12 +94,23 @@ public class DevPlayer {
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onPlayerRenderPre(RenderLivingEvent.Pre event) {
-        if(nameIsGood(event.entity)){
+        if (namesForTransparency(event.entity)) {
+            GL11.glColor4f(1.0F, 1.0F, 1.0F, 0.75F);
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            resetRender = true;
+        }
+
+        if (namesForRainbow(event.entity)) {
             Color color;
             float hue = ((float) event.entity.worldObj.getTotalWorldTime() % 256) / 256;
             color = new Color(Color.HSBtoRGB(hue + (renderTick / 256), 1, 1));
 
-            GL11.glColor4f(((float)color.getRed()) / 256, ((float)color.getGreen()) / 256, ((float)color.getBlue()) / 256, 0.75F);
+            float r = ((((float) color.getRed() / 256.0F) / 2.0F) + 0.5F);
+            float g = ((((float) color.getGreen() / 256.0F) / 2.0F) + 0.5F);
+            float b = ((((float) color.getBlue() / 256.0F) / 2.0F) + 0.5F);
+
+            GL11.glColor4f((r > 1.0F ? 1.0F : r), (g > 1.0F ? 1.0F : g), (b > 1.0F ? 1.0F : b), 1.0F);
             GL11.glEnable(GL11.GL_BLEND);
             GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
             resetRender = true;
@@ -119,14 +119,14 @@ public class DevPlayer {
 
     @SubscribeEvent
     public void onPlayerRenderPost(RenderLivingEvent.Post event) {
-        if (nameIsGood(event.entity)){
+        if (namesForTransparency(event.entity) || namesForRainbow(event.entity)) {
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
             GL11.glDisable(GL11.GL_BLEND);
             resetRender = false;
         }
     }
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    @SubscribeEvent
     public void onPlayerRenderSpecialPre(RenderPlayerEvent.Specials.Pre event) {
         Collection<IDevRenderer> renders = renderMap.get(event.entityPlayer.getUniqueID());
         for (IDevRenderer r : renders) {
@@ -142,11 +142,16 @@ public class DevPlayer {
         }
     }
 
-    private boolean nameIsGood(Entity entity){
-        if(EnumChatFormatting.getTextWithoutFormattingCodes(entity.getCommandSenderName()).equals("Cricket")
-            || EnumChatFormatting.getTextWithoutFormattingCodes(entity.getCommandSenderName()).equals("Drullkus")
-            || EnumChatFormatting.getTextWithoutFormattingCodes(entity.getCommandSenderName()).equals("minecreatr"))
-        {
+    private boolean namesForTransparency(Entity entity) {
+        if (EnumChatFormatting.getTextWithoutFormattingCodes(entity.getCommandSenderName()).equals("Cricket") || EnumChatFormatting.getTextWithoutFormattingCodes(entity.getCommandSenderName())
+            .equals("minecreatr")) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean namesForRainbow(Entity entity) {
+        if (EnumChatFormatting.getTextWithoutFormattingCodes(entity.getCommandSenderName()).equals("Drullkus")) {
             return true;
         }
         return false;
