2e9e427e48f2ea49b0fdb0c7af0fbf432686fd72
tterrag1098 <tterrag1098@gmail.com>
2016-02-15 00:40:58 -0500

    This really needed a TODO...

--- src/main/java/team/chisel/client/render/Quad.java
+++ src/main/java/team/chisel/client/render/Quad.java
@@ -101,6 +101,7 @@ public class Quad {
             float maxU = minU + (width * submap.getWidth());
             float maxV = minV + (height * submap.getHeight());
 
+            // TODO this is horrid
             return new UVs(other, 
                     new Vector2f(data[0].x == this.minU ? minU : maxU, data[0].y == this.minV ? minV : maxV), 
                     new Vector2f(data[1].x == this.minU ? minU : maxU, data[1].y == this.minV ? minV : maxV), 
