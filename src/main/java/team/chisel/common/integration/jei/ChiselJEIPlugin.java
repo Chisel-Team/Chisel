1c2cccccbf31ac231462b8b09917824d4918df70
tterrag1098 <tterrag1098@gmail.com>
2016-03-24 03:34:26 -0400

    Fix JEI plugin

--- src/main/java/team/chisel/common/integration/jei/ChiselJEIPlugin.java
+++ src/main/java/team/chisel/common/integration/jei/ChiselJEIPlugin.java
@@ -2,7 +2,6 @@ package team.chisel.common.integration.jei;
 
 import java.util.stream.Collectors;
 
-import mezz.jei.api.IJeiHelpers;
 import mezz.jei.api.IJeiRuntime;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
@@ -13,15 +12,9 @@ import team.chisel.common.integration.jei.ChiselRecipeHandler.CarvingGroupWrappe
 @JEIPlugin
 public class ChiselJEIPlugin implements IModPlugin {
 
-    private IJeiHelpers jeiHelpers;
-
-    public void onJeiHelpersAvailable(IJeiHelpers jeiHelpers){
-        this.jeiHelpers = jeiHelpers;
-    }
-
     @Override
     public void register(IModRegistry registry){
-        registry.addRecipeCategories(new ChiselRecipeCategory(jeiHelpers.getGuiHelper()));
+        registry.addRecipeCategories(new ChiselRecipeCategory(registry.getJeiHelpers().getGuiHelper()));
         registry.addRecipeHandlers(new ChiselRecipeHandler());
         
         registry.addRecipes(Carving.chisel.getSortedGroupNames().stream().map(s -> Carving.chisel.getGroup(s)).map(g -> new CarvingGroupWrapper(g)).collect(Collectors.toList()));
