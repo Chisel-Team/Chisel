3fca3fb95135afcbe9ac4824592153bb58c06810
tterrag1098 <tterrag1098@gmail.com>
2016-10-10 22:00:17 -0400

    Move away from "ctm:" prefix for models, now uses embedded "ctm" object

--- src/main/java/team/chisel/client/render/ModelLoaderChisel.java
+++ src/main/java/team/chisel/client/render/ModelLoaderChisel.java
@@ -1,9 +1,20 @@
 package team.chisel.client.render;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+import org.apache.logging.log4j.message.MessageFormatMessage;
+
+import com.google.common.base.Throwables;
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import net.minecraft.client.renderer.block.model.ModelBlockDefinition;
 import net.minecraft.client.resources.IResource;
@@ -11,11 +22,7 @@ import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.ICustomModelLoader;
 import net.minecraftforge.client.model.IModel;
-
-import com.google.common.base.Throwables;
-import com.google.common.collect.Maps;
-import com.google.gson.Gson;
-
+import team.chisel.Chisel;
 
 public class ModelLoaderChisel implements ICustomModelLoader {
 
@@ -27,14 +34,21 @@ public class ModelLoaderChisel implements ICustomModelLoader {
     private Map<ResourceLocation, ModelChisel> loadedModels = Maps.newHashMap();
     
     @Override
-    public void onResourceManagerReload(IResourceManager resourceManager) {
+    public void onResourceManagerReload(@Nonnull IResourceManager resourceManager) {
         this.manager = resourceManager;
         loadedModels.clear();
     }
 
     @Override
     public boolean accepts(ResourceLocation modelLocation) {
-        return modelLocation.getResourceDomain().equals("ctm");
+        // Old way of doing things
+        if (modelLocation.getResourceDomain().equals("ctm")) {
+            return true;
+        }
+        
+        // New way, embedded "ctm" object in model file
+        JsonElement json = getJSON(modelLocation);
+        return json.isJsonObject() && json.getAsJsonObject().has("ctm");
     }
 
     @Override
@@ -46,20 +60,49 @@ public class ModelLoaderChisel implements ICustomModelLoader {
         }
         return model;
     }
+    
+    @SuppressWarnings("null")
+    private @Nonnull JsonElement getJSON(ResourceLocation modelLocation) {
+        ResourceLocation absolute = new ResourceLocation(modelLocation.getResourceDomain(), modelLocation.getResourcePath() + ".json");
+
+        try {
+            IResource resource = manager.getResource(absolute);
+            JsonElement ele = new JsonParser().parse(new InputStreamReader(resource.getInputStream()));
+            if (ele != null) {
+                return ele;
+            }
+        } catch (IOException e) {}
+        
+        return JsonNull.INSTANCE;
+    }
 
     private ModelChisel loadFromFile(ResourceLocation res) {
-        String domain = res.getResourcePath();
-        String path = "models/block/" + domain.substring(domain.indexOf(":") + 1, domain.length());
-        domain = domain.substring(0, domain.indexOf(":"));
-        domain = domain.substring(domain.lastIndexOf("/") + 1, domain.length());
-        res = new ResourceLocation(domain, path);
-        try {
-            IResource resource = manager.getResource(res);
-            return gson.fromJson(new InputStreamReader(resource.getInputStream()), ModelChisel.class);
-        } catch (FileNotFoundException f) {
-            return gson.fromJson(String.format(DEFAULT_MODEL, res.toString().replace("models/block/", "").replace(".json", ".cf")), ModelChisel.class);
-        } catch (IOException e) {
-            throw Throwables.propagate(e);
+        if (res.getResourceDomain().equals("ctm")) {
+            @Nonnull String domain = res.getResourcePath();
+            String path = "models/block/" + domain.substring(domain.indexOf(":") + 1, domain.length());
+            domain = domain.substring(0, domain.indexOf(":"));
+            domain = domain.substring(domain.lastIndexOf("/") + 1, domain.length());
+            res = new ResourceLocation(domain, path);
+        }
+        
+        JsonElement json = getJSON(res);
+
+        if (json.isJsonObject()) {
+            JsonObject obj = json.getAsJsonObject();
+            if (obj.has("ctm")) {
+                JsonElement ctm = obj.get("ctm");
+                if (ctm.isJsonObject()) {
+                    obj = ctm.getAsJsonObject();
+                } else if (ctm.isJsonPrimitive() && ctm.getAsString().equals("parent")) {
+                    return loadFromFile(new ResourceLocation(obj.get("parent").getAsString()));
+                }
+            }
+            return gson.fromJson(obj, ModelChisel.class);
+        } else if (json.isJsonNull()) {
+            Chisel.debug("Substituting default model json for missing file " + res);
+            return gson.fromJson(String.format(DEFAULT_MODEL, res.toString().replace("models/block/", "").concat(".cf")), ModelChisel.class);
+        } else {
+            throw new IllegalArgumentException("Found invalid JSON information \"" + json + "\" loading model " + res);
         }
     }
 }
