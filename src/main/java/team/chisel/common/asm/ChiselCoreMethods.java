5fc004edde47cbb3d7fe11e2bfe02e6e9357eef6
tterrag1098 <tterrag1098@gmail.com>
2016-12-27 20:23:36 -0500

    update to 1.11.2

--- src/main/java/team/chisel/common/asm/ChiselCoreMethods.java
+++ src/main/java/team/chisel/common/asm/ChiselCoreMethods.java
@@ -12,13 +12,12 @@ import team.chisel.client.render.ModelChiselBlock;
 
 public class ChiselCoreMethods {
     
-    @SuppressWarnings("deprecation")
     public static boolean canRenderInLayer(@Nonnull IBlockState state, @Nonnull BlockRenderLayer layer) {
         IBakedModel model = Minecraft.getMinecraft().getBlockRendererDispatcher().getModelForState(state);
         if (model instanceof ModelChiselBlock) {
             return ((ModelChiselBlock) model).getModel().getChiselTextures().stream().map(t -> t.getLayer()).collect(Collectors.toList()).contains(layer);
         }
-        return state.getBlock().canRenderInLayer(layer);
+        return state.getBlock().canRenderInLayer(state, layer);
     }
 
 }
