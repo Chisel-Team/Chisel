fdec1fa1bdf91898cce4aaf8ea8f8f7407555894
tterrag1098 <tterrag1098@gmail.com>
2017-02-15 18:03:45 -0500

    Fix #353 lag when updating chunks

--- src/main/java/team/chisel/common/asm/ChiselCoreMethods.java
+++ src/main/java/team/chisel/common/asm/ChiselCoreMethods.java
@@ -2,14 +2,13 @@ package team.chisel.common.asm;
 
 import java.util.Collections;
 import java.util.EnumSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.WeakHashMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
-
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 
 import lombok.SneakyThrows;
 import net.minecraft.block.state.IBlockState;
@@ -23,36 +22,34 @@ import team.chisel.common.util.ProfileUtil;
 
 public class ChiselCoreMethods {
     
-    private static final LoadingCache<IBlockState, Set<BlockRenderLayer>> cache = CacheBuilder.newBuilder().build(new CacheLoader<IBlockState, Set<BlockRenderLayer>>() {
-        
-        @SuppressWarnings("null")
-        @Override
-        public @Nonnull Set<BlockRenderLayer> load(@Nonnull IBlockState state) throws Exception {
-            IBakedModel model = Minecraft.getMinecraft().getBlockRendererDispatcher().getModelForState(state);
-            if (model instanceof ModelChiselBlock) {
-                return ((ModelChiselBlock) model).getModel().getChiselTextures().stream()
-                        .map(IChiselTexture::getLayer)
-                        .collect(Collectors.toCollection(() -> EnumSet.noneOf(BlockRenderLayer.class)));
-            }
-            return Collections.emptySet();
+    private static final Map<IBakedModel, Set<BlockRenderLayer>> cache = new WeakHashMap<>();
+      
+    private static final Function<IBakedModel, Set<BlockRenderLayer>> loadFunc = (model) -> {
+        if (model instanceof ModelChiselBlock) {
+            return ((ModelChiselBlock) model).getModel().getChiselTextures().stream()
+                    .map(IChiselTexture::getLayer)
+                    .collect(Collectors.toCollection(() -> EnumSet.noneOf(BlockRenderLayer.class)));
         }
-    });
-    
+        return Collections.emptySet();
+    };
+
     static {
-        ((SimpleReloadableResourceManager)Minecraft.getMinecraft().getResourceManager()).registerReloadListener(r -> cache.invalidateAll());
+        ((SimpleReloadableResourceManager)Minecraft.getMinecraft().getResourceManager()).registerReloadListener(r -> cache.clear());
     }
 
     @SuppressWarnings("deprecation")
     @SneakyThrows
     public static boolean canRenderInLayer(@Nonnull IBlockState state, @Nonnull BlockRenderLayer layer) {
         ProfileUtil.start("chisel_render_in_layer");
-        Set<BlockRenderLayer> layers = cache.get(state);
-        if (layers.isEmpty()) {
-            ProfileUtil.end();
-            return state.getBlock().canRenderInLayer(layer);
+        IBakedModel model = Minecraft.getMinecraft().getBlockRendererDispatcher().getModelForState(state);
+
+        boolean ret;
+        if (model instanceof ModelChiselBlock) {
+            ret = cache.computeIfAbsent(model, loadFunc).contains(layer);
         } else {
-            ProfileUtil.end();
-            return layers.contains(layer);
+            ret = state.getBlock().canRenderInLayer(layer);
         }
+        ProfileUtil.end();
+        return ret;
     }
 }
