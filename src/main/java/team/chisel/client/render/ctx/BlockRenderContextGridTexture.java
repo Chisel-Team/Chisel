f807af7a1ba83a908f553726b67376184e8ccd26
tterrag1098 <tterrag1098@gmail.com>
2017-03-26 20:03:53 -0400

    Fix #399 offset wand not working

--- src/main/java/team/chisel/client/render/ctx/BlockRenderContextGridTexture.java
+++ src/main/java/team/chisel/client/render/ctx/BlockRenderContextGridTexture.java
@@ -19,12 +19,12 @@ public abstract class BlockRenderContextGridTexture extends BlockRenderContextPo
     
     public static class Patterned extends BlockRenderContextGridTexture {
 
-        public Patterned(BlockPos pos, ChiselTextureMap tex) {
-            super(pos, tex);
+        public Patterned(BlockPos pos, ChiselTextureMap tex, boolean applyOffset) {
+            super(pos, tex, applyOffset);
         }
 
         @Override
-        protected Point2i calculateTextureCoord(BlockPos pos, ChiselTextureMap tex, EnumFacing side) {
+        protected Point2i calculateTextureCoord(BlockPos pos, int w, int h, EnumFacing side) {
             int x = pos.getX();
             int y = pos.getY();
             int z = pos.getZ();
@@ -35,29 +35,29 @@ public abstract class BlockRenderContextGridTexture extends BlockRenderContextPo
             // Negate the y coordinate when calculating non-vertical directions, otherwise it is reverse order
             if (side.getAxis().isVertical()) {
                 // DOWN || UP
-                tx = x % tex.getXSize();
-                ty = (side.getFrontOffsetY() * z + 1) % tex.getYSize();
+                tx = x % w;
+                ty = (side.getFrontOffsetY() * z + 1) % h;
             } else if (side.getAxis() == Axis.Z) {
                 // NORTH || SOUTH
-                tx = x % tex.getXSize();
-                ty = -y % tex.getYSize();
+                tx = x % w;
+                ty = -y % h;
             } else {
                 // WEST || EAST
-                tx = (z + 1) % tex.getXSize();
-                ty = -y % tex.getYSize();
+                tx = (z + 1) % w;
+                ty = -y % h;
             }
 
             // Reverse x order for north and east
             if (side == EnumFacing.NORTH || side == EnumFacing.EAST) {
-                tx = (tex.getXSize() - tx - 1) % tex.getXSize();
+                tx = (w - tx - 1) % w;
             }
 
             // Remainder can produce negative values, so wrap around
             if (tx < 0) {
-                tx += tex.getXSize();
+                tx += w;
             }
             if (ty < 0) {
-                ty += tex.getYSize();
+                ty += h;
             }
             
             return new Point2i(tx, ty);
@@ -68,18 +68,15 @@ public abstract class BlockRenderContextGridTexture extends BlockRenderContextPo
         
         private static final java.util.Random rand = new java.util.Random();
         
-        public Random(BlockPos pos, ChiselTextureMap tex) {
-            super(pos, tex);
+        public Random(BlockPos pos, ChiselTextureMap tex, boolean applyOffset) {
+            super(pos, tex, applyOffset);
         }
 
         @Override
-        protected Point2i calculateTextureCoord(BlockPos pos, ChiselTextureMap tex, EnumFacing side) {
+        protected Point2i calculateTextureCoord(BlockPos pos, int w, int h, EnumFacing side) {
 
             rand.setSeed(MathHelper.getPositionRandom(pos) + side.ordinal());
             rand.nextBoolean();
-
-            int w = tex.getXSize();
-            int h = tex.getYSize();
 
             int tx = rand.nextInt(w) + 1;
             int ty = rand.nextInt(h) + 1;
@@ -88,20 +85,24 @@ public abstract class BlockRenderContextGridTexture extends BlockRenderContextPo
         }
     }
     
-    private final EnumMap<EnumFacing, Point2i> textureCoords = new EnumMap<>(EnumFacing.class);
+    private final EnumMap<EnumFacing, Point2i> textureCoords = new EnumMap<>(EnumFacing.class);    
     private final long serialized;
-    
+
     @SuppressWarnings("null")
-    public BlockRenderContextGridTexture(BlockPos pos, ChiselTextureMap tex) {
+    public BlockRenderContextGridTexture(BlockPos pos, ChiselTextureMap tex, boolean applyOffset) {
         super(pos);
 
         // Since we can only return a long, we must limit to 10 bits of data per face = 60 bits
         Preconditions.checkArgument(tex.getXSize() * tex.getYSize() < 1024, "V* Texture size too large for texture %s", tex.getParticle());
-        long serialized = 0;
         
+        if (applyOffset) {
+            applyOffset();
+        }
+        
+        long serialized = 0;
         for (@Nonnull EnumFacing side : EnumFacing.VALUES) {
 
-            Point2i coords = calculateTextureCoord(pos, tex, side);
+            Point2i coords = calculateTextureCoord(position, tex.getXSize(), tex.getYSize(), side);
             textureCoords.put(side, coords);
             
             // Calculate a unique index for a submap (x + (y * x-size)), then shift it left by the max bit storage (10 bits = 1024 unique indices)
@@ -111,7 +112,7 @@ public abstract class BlockRenderContextGridTexture extends BlockRenderContextPo
         this.serialized = serialized;
     }
     
-    protected abstract Point2i calculateTextureCoord(BlockPos pos, ChiselTextureMap tex, EnumFacing side);
+    protected abstract Point2i calculateTextureCoord(BlockPos pos, int w, int h, EnumFacing side);
     
     @SuppressWarnings("null")
     public Point2i getTextureCoords(EnumFacing side) {
