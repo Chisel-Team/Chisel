037d80ce1f68025ce3fff026ca88cbfb0968bc68
tterrag1098 <tterrag1098@gmail.com>
2017-03-27 23:36:55 -0400

    Fix #296 re-implement WAILA (HWYLA) support

---
+++ src/main/java/team/chisel/common/integration/waila/ChiselDataHandler.java
@@ -0,0 +1,60 @@
+package team.chisel.common.integration.waila;
+
+import java.util.List;
+
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import mcp.mobius.waila.api.IWailaPlugin;
+import mcp.mobius.waila.api.IWailaRegistrar;
+import mcp.mobius.waila.api.WailaPlugin;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import team.chisel.api.block.ICarvable;
+import team.chisel.common.block.ItemChiselBlock;
+
+@WailaPlugin
+public class ChiselDataHandler implements IWailaPlugin, IWailaDataProvider {
+
+    @Override
+    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler configHandler) {
+        return null;
+    }
+
+    @Override
+    public List<String> getWailaHead(ItemStack stack, List<String> strings, IWailaDataAccessor accessor, IWailaConfigHandler configHandler) {
+        return strings;
+    }
+
+    @SuppressWarnings("null")
+    @Override
+    public List<String> getWailaBody(ItemStack stack, List<String> strings, IWailaDataAccessor accessor, IWailaConfigHandler configHandler) {
+        if (accessor.getBlock() instanceof ICarvable) {
+            ItemChiselBlock item = (ItemChiselBlock) ForgeRegistries.ITEMS.getValue(accessor.getBlock().getRegistryName());
+            ICarvable block = (ICarvable) accessor.getBlock();
+            int variation = block.getVariationIndex(accessor.getBlockState());
+            item.addInformation(new ItemStack(item, 1, variation), accessor.getPlayer(), strings, false);
+        }
+        return strings;
+    }
+
+    @Override
+    public List<String> getWailaTail(ItemStack stack, List<String> strings, IWailaDataAccessor accessor, IWailaConfigHandler configHandler) {
+        return strings;
+    }
+
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
+        return tag;
+    }
+
+    @Override
+    public void register(IWailaRegistrar registrar) {
+        registrar.registerBodyProvider(this, ICarvable.class);
+    }
+}
