99266ed12c30c6022932b176ba3f4a2b19cd88bd
tterrag1098 <tterrag1098@gmail.com>
2017-05-07 16:43:04 -0400

    Add support for weighted random models

--- src/main/java/team/chisel/common/asm/ChiselCoreMethods.java
+++ src/main/java/team/chisel/common/asm/ChiselCoreMethods.java
@@ -6,6 +6,7 @@ import lombok.SneakyThrows;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.block.model.WeightedBakedModel;
 import net.minecraft.util.BlockRenderLayer;
 import team.chisel.client.render.AbstractChiselBakedModel;
 import team.chisel.common.util.ProfileUtil;
@@ -17,7 +18,10 @@ public class ChiselCoreMethods {
     public static boolean canRenderInLayer(@Nonnull IBlockState state, @Nonnull BlockRenderLayer layer) {
         ProfileUtil.start("chisel_render_in_layer");
         IBakedModel model = Minecraft.getMinecraft().getBlockRendererDispatcher().getModelForState(state);
-
+        if (model instanceof WeightedBakedModel) {
+            model = ((WeightedBakedModel)model).baseModel;
+        }
+        
         boolean ret;
         if (model instanceof AbstractChiselBakedModel) {
             ret = ((AbstractChiselBakedModel)model).getModel().canRenderInLayer(state, layer);
