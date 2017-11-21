2423ee5b2e33aa383ef45d2362d59dc8146adb7d
tterrag <tterrag1098@gmail.com>
2017-11-21 04:01:59 -0500

    Fix missing tooltip on iChisel, improve readability of chisel tooltips

--- src/main/java/team/chisel/common/item/ItemChisel.java
+++ src/main/java/team/chisel/common/item/ItemChisel.java
@@ -19,6 +19,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import team.chisel.Chisel;
 import team.chisel.api.IChiselGuiType;
@@ -28,6 +29,7 @@ import team.chisel.api.carving.ICarvingVariation;
 import team.chisel.api.carving.IChiselMode;
 import team.chisel.common.config.Configurations;
 import team.chisel.common.init.ChiselTabs;
+import team.chisel.common.util.NBTUtil;
 
 public class ItemChisel extends Item implements IChiselItem {
     
@@ -88,18 +90,15 @@ public class ItemChisel extends Item implements IChiselItem {
     @Override
     public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean held) {
         String base = "item.chisel.chisel.desc.";
-        String gui = I18n.format(base + "gui");
-        String lc1 = I18n.format(base + "lc1");
-        String lc2 = I18n.format(base + "lc2");
-        String modes = I18n.format(base + "modes");
-        list.add(gui);
-        if (type == ChiselType.DIAMOND || Configurations.ironChiselCanLeftClick) {
-            list.add(lc1);
-            list.add(lc2);
+        list.add(I18n.format(base + "gui", TextFormatting.AQUA, TextFormatting.GRAY));
+        if (type != ChiselType.IRON || Configurations.ironChiselCanLeftClick) {
+            list.add(I18n.format(base + "lc1", TextFormatting.AQUA, TextFormatting.GRAY));
+            list.add(I18n.format(base + "lc2", TextFormatting.AQUA, TextFormatting.GRAY));
         }
-        if (type == ChiselType.DIAMOND || Configurations.ironChiselHasModes) {
+        if (type != ChiselType.IRON || Configurations.ironChiselHasModes) {
             list.add("");
-            list.add(modes);
+            list.add(I18n.format(base + "modes"));
+            list.add(I18n.format(base + "modes.selected", TextFormatting.GREEN + I18n.format(NBTUtil.getChiselMode(stack).getUnlocName() + ".name")));
         }
     }
 
