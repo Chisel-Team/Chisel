898feb996e70ed4ef9c0b801bef89fc4859439ac
tterrag <tterrag1098@gmail.com>
2017-11-27 03:10:40 -0500

    Remove ghostCloud config

--- src/main/java/team/chisel/common/config/Configurations.java
+++ src/main/java/team/chisel/common/config/Configurations.java
@@ -14,7 +14,6 @@ public class Configurations {
     public static boolean configExists;
 
     public static double concreteVelocityMult;
-    public static boolean ghostCloud;
     public static int factoryBlockAmount;
     public static boolean allowMossy;
     public static boolean allowSmoothStone;
@@ -55,7 +54,6 @@ public class Configurations {
         /* general */
         category = "general";
         concreteVelocityMult = config.get(category, "concreteVelocityMult", 1.35, "The factor that concrete_white increases your velocity. Default is 1.35, set to 1 for no change.").getDouble();
-        ghostCloud = config.get(category, "doesCloudRenderLikeGhost", true).getBoolean(true);
         allowMossy = config.get(category, "allowBrickToMossyInChisel", true, "If true, you can chisel stone brick to mossy stone brick.").getBoolean(true);
         chiselRecipe = config.get(category, "chiselAlternateRecipe", false, "Use alternative crafting recipe for the chisel").getBoolean(false);
         chiselStoneToCobbleBricks = config.get(category, "chiselStoneToStoneBricks", true, "Allow stone to be chiseled to/from stone bricks.").getBoolean(true);
