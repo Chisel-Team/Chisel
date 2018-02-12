aefcf71b6c7e0852b55162af15e11fc3d8fe2004
Nicholas Ignoffo <nick@tehnut.info>
2018-02-12 15:54:14 -0800

    Don't register disabled features with BM

--- src/main/java/team/chisel/common/integration/bloodmagic/ChiselBMPlugin.java
+++ src/main/java/team/chisel/common/integration/bloodmagic/ChiselBMPlugin.java
@@ -3,6 +3,7 @@ package team.chisel.common.integration.bloodmagic;
 import WayofTime.bloodmagic.api.BloodMagicPlugin;
 import WayofTime.bloodmagic.api.IBloodMagicAPI;
 import WayofTime.bloodmagic.api.IBloodMagicPlugin;
+import team.chisel.Features;
 import team.chisel.common.block.BlockCarvable;
 import team.chisel.common.init.ChiselBlocks;
 
@@ -15,28 +16,30 @@ public class ChiselBMPlugin implements IBloodMagicPlugin {
     public void register(IBloodMagicAPI api) {
         ChiselBMPlugin.api = api;
 
-        setAltarComponent(ChiselBlocks.bloodmagic, "BLOODRUNE");
-        setAltarComponent(ChiselBlocks.glowstone, "GLOWSTONE");
-        setAltarComponent(ChiselBlocks.glowstone1, "GLOWSTONE");
-        setAltarComponent(ChiselBlocks.glowstone2, "GLOWSTONE");
+        setAltarComponent(Features.BLOOD_MAGIC, ChiselBlocks.bloodmagic, "BLOODRUNE");
+        setAltarComponent(Features.GLOWSTONE, ChiselBlocks.glowstone, "GLOWSTONE");
+        setAltarComponent(Features.GLOWSTONE, ChiselBlocks.glowstone1, "GLOWSTONE");
+        setAltarComponent(Features.GLOWSTONE, ChiselBlocks.glowstone2, "GLOWSTONE");
 
-        setTranquility(ChiselBlocks.dirt, "EARTHEN", 0.25D);
-        setTranquility(ChiselBlocks.netherrack, "FIRE", 0.5D);
-        setTranquility(ChiselBlocks.lavastone, "LAVA", 1.2D);
-        setTranquility(ChiselBlocks.lavastone1, "LAVA", 1.2D);
-        setTranquility(ChiselBlocks.lavastone2, "LAVA", 1.2D);
-        setTranquility(ChiselBlocks.waterstone, "WATER", 1.0D);
-        setTranquility(ChiselBlocks.waterstone1, "WATER", 1.0D);
-        setTranquility(ChiselBlocks.waterstone2, "WATER", 1.0D);
+        setTranquility(Features.DIRT, ChiselBlocks.dirt, "EARTHEN", 0.25D);
+        setTranquility(Features.NETHERRACK, ChiselBlocks.netherrack, "FIRE", 0.5D);
+        setTranquility(Features.LAVASTONE, ChiselBlocks.lavastone, "LAVA", 1.2D);
+        setTranquility(Features.LAVASTONE, ChiselBlocks.lavastone1, "LAVA", 1.2D);
+        setTranquility(Features.LAVASTONE, ChiselBlocks.lavastone2, "LAVA", 1.2D);
+        setTranquility(Features.WATERSTONE, ChiselBlocks.waterstone, "WATER", 1.0D);
+        setTranquility(Features.WATERSTONE, ChiselBlocks.waterstone1, "WATER", 1.0D);
+        setTranquility(Features.WATERSTONE, ChiselBlocks.waterstone2, "WATER", 1.0D);
     }
 
-    private static void setAltarComponent(BlockCarvable block, String componentType) {
-        for (int i = 0; i < block.getTotalVariations(); i++)
-            api.registerAltarComponent(block.getStateFromMeta(i), componentType);
+    private static void setAltarComponent(Features feature, BlockCarvable block, String componentType) {
+        if (feature.enabled())
+            for (int i = 0; i < block.getTotalVariations(); i++)
+                api.registerAltarComponent(block.getStateFromMeta(i), componentType);
     }
 
-    private static void setTranquility(BlockCarvable block, String tranquilityType, double value) {
-        for (int i = 0; i < block.getTotalVariations(); i++)
-            api.getValueManager().setTranquility(block.getStateFromMeta(i), tranquilityType, value);
+    private static void setTranquility(Features feature, BlockCarvable block, String tranquilityType, double value) {
+        if (feature.enabled())
+            for (int i = 0; i < block.getTotalVariations(); i++)
+                api.getValueManager().setTranquility(block.getStateFromMeta(i), tranquilityType, value);
     }
 }
