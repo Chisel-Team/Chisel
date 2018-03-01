37c1fcc3e2fc6de89e2c193f6806f8cac46c9f8d
Drullkus <ericsplanet@mac.com>
2018-03-01 02:59:29 -0800

    More model fixing

--- src/main/java/team/chisel/common/Reference.java
+++ src/main/java/team/chisel/common/Reference.java
@@ -6,12 +6,12 @@ import javax.annotation.ParametersAreNonnullByDefault;
  * Reference class for different mod information
  */
 @ParametersAreNonnullByDefault
+@SuppressWarnings("UnnecessaryInterfaceModifier")
 public interface Reference {
-
     public static final String MOD_ID = "chisel";
 
     public static final String MOD_NAME = "Chisel";
-    public static final String VERSION = "MC1.10.2-0.0.14.32";
+    public static final String VERSION = "@VERSION@";
 
     public static final String CLIENT_PROXY = "team.chisel.client.ClientProxy";
     public static final String COMMON_PROXY = "team.chisel.common.CommonProxy";
