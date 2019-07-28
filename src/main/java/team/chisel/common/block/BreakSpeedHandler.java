4711df96cc98d4754bfd0ecf7044f4976562d068
tterrag1098 <tterrag1098@gmail.com>
2019-07-27 23:14:42 -0700

    Fix chiseled wool not breaking faster with shears

---
+++ src/main/java/team/chisel/common/block/BreakSpeedHandler.java
@@ -0,0 +1,19 @@
+package team.chisel.common.block;
+
+import net.minecraft.block.Block;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import team.chisel.Chisel;
+
+@EventBusSubscriber(modid = Chisel.MOD_ID)
+public class BreakSpeedHandler {
+
+    @SubscribeEvent
+    public static void onBreakSpeed(PlayerEvent.BreakSpeed event) {
+        Block block = event.getState().getBlock();
+        if (block instanceof BlockCarvable && block.getRegistryName().getResourcePath().startsWith("wool_")) {
+            event.setNewSpeed(event.getNewSpeed() * 5);
+        }
+    }
+}
