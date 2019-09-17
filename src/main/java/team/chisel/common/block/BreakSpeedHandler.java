694985bd14684ad66c5ce4c10fc2dd0a39a1e988
tterrag <tterrag1098@gmail.com>
2019-09-17 00:49:45 -0400

    Fix wool always breaking fast regardless of tool

--- src/main/java/team/chisel/common/block/BreakSpeedHandler.java
+++ src/main/java/team/chisel/common/block/BreakSpeedHandler.java
@@ -1,6 +1,7 @@
 package team.chisel.common.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -13,7 +14,7 @@ public class BreakSpeedHandler {
     public static void onBreakSpeed(PlayerEvent.BreakSpeed event) {
         Block block = event.getState().getBlock();
         if (block instanceof BlockCarvable && block.getRegistryName().getResourcePath().startsWith("wool_")) {
-            event.setNewSpeed(event.getNewSpeed() * 5);
+            event.setNewSpeed(event.getNewSpeed() * event.getEntityPlayer().getHeldItemMainhand().getStrVsBlock(Blocks.WOOL.getDefaultState()));
         }
     }
 }
