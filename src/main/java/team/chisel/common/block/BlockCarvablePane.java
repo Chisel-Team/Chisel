b54df537b51ca096e0e9c4b0d7771578c3b23434
tterrag <tterrag1098@gmail.com>
2019-10-13 01:31:01 -0400

    Fix pane edge rotation weirdness, add better culling for pane top/bottom

--- src/main/java/team/chisel/common/block/BlockCarvablePane.java
+++ src/main/java/team/chisel/common/block/BlockCarvablePane.java
@@ -1,7 +1,5 @@
 package team.chisel.common.block;
 
-import java.util.List;
-
 import javax.annotation.ParametersAreNonnullByDefault;
 
 import lombok.Getter;
@@ -11,12 +9,14 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumFacing.Axis;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -129,9 +129,18 @@ public class BlockCarvablePane extends BlockPane implements ICarvable {
         return this.index;
     }
 
-    @SuppressWarnings("null") // No type annotations
     @Override
     public VariationData getVariationData(int meta) {
         return this.variations[MathHelper.clamp(meta, 0, this.variations.length - 1)];
+    }
+    
+    @SideOnly(Side.CLIENT)
+    @Override
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+        if (!super.shouldSideBeRendered(blockState, blockAccess, pos, side)) {
+            if (side.getAxis() != Axis.Y) return false;
+            return blockAccess.getBlockState(pos.offset(side)).getActualState(blockAccess, pos.offset(side)) != blockState;
+        }
+        return true;
     }
 }
