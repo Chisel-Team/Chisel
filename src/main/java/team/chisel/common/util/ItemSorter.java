659c4f6a4acc5d71e8590e818b662e8e2a5fc91a
tterrag <tterrag1098@gmail.com>
2020-03-18 23:39:58 -0400

    Fix sorting of vanilla variants

--- src/main/java/team/chisel/common/util/ItemSorter.java
+++ src/main/java/team/chisel/common/util/ItemSorter.java
@@ -2,6 +2,7 @@ package team.chisel.common.util;
 
 import java.util.Comparator;
 
+import net.minecraft.item.Item;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
 
@@ -20,19 +21,23 @@ public class ItemSorter {
     }
     
     public static <T extends IItemProvider> Comparator<T> alphabeticVanillaFirst() {
-        return byName(vanillaFirst().thenComparing(alphabeticByName()));
+        return ItemSorter.<T>vanillaFirst().thenComparing(ItemSorter.<T>byName(alphabeticByName()));
     }
     
     public static <T extends IItemProvider> Comparator<T> variantOrder() {
-        return byName(vanillaFirst().thenComparing(rawFirst()).thenComparing(alphabeticByName()));
+        return ItemSorter.<T>vanillaFirst().thenComparing(byName(rawFirst().thenComparing(alphabeticByName())));
     }
 
-    private static Comparator<ResourceLocation> vanillaFirst() {
-        return Comparator.comparing(r -> r.getNamespace(), (n1, n2) ->
-                n1.equals("minecraft") && n2.equals("minecraft") ? 0 :
-                n1.equals("minecraft") ? -1 :
-                n2.equals("minecraft") ? 1 :
-                0);
+    private static <T extends IItemProvider> Comparator<T> vanillaFirst() {
+        return (i1, i2) -> {
+            String n1 = i1.asItem().getRegistryName().getNamespace();
+            String n2 = i2.asItem().getRegistryName().getNamespace();
+            if (n1.equals("minecraft") && n2.equals("minecraft")) {
+                return Integer.compare(Item.getIdFromItem(i1.asItem()), Item.getIdFromItem(i2.asItem()));
+            } else {
+                return n1.equals("minecraft") ? -1 : n2.equals("minecraft") ? 1 : 0;
+            }
+        };
     }
     
     private static Comparator<ResourceLocation> rawFirst() {
